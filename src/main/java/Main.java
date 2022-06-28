4a30c7f3e3df10e3efe24b84fe7551dc58172789
GrayHead <ss.zhuravlov@gmail.com>
2022-06-28 11:47:47 +0300

    clear

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -15,48 +15,15 @@ public class Main {
     public static void main(String[] args) {
 
         StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-        Metadata metadata = new MetadataSources(serviceRegistry).addAnnotatedClass(Passport.class).addAnnotatedClass(User.class).getMetadataBuilder().build();
+        Metadata metadata = new MetadataSources(serviceRegistry).addAnnotatedClass(null).addAnnotatedClass(null).getMetadataBuilder().build();
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
         Session session = sessionFactory.openSession();
 
         session.beginTransaction();
 
-        session.save(new User("kokos", new Passport("qywerygafdua"), Gendrer.FEMALE, Arrays.asList("java","html")));
-        session.save(new User("ananas", new Passport("oiyiuehg"), Gendrer.MALE , Arrays.asList("java","js")));
-        session.save(new User("banan", new Passport("riyehdogfhi"), Gendrer.MALE, Arrays.asList("java","html")));
-        session.save(new User("tomat", new Passport("987f8tgf0g"), Gendrer.FEMALE, Arrays.asList("java","mysql")));
-        session.save(new User("potatos", new Passport("ogy8egy8e7g8"), Gendrer.FEMALE , Arrays.asList("java","mongo")));
-        session.save(new User("mango", new Passport("rty8erg8g82r7f"), Gendrer.FEMALE , Arrays.asList("java")));
-
 
         session.getTransaction().commit();
 
-
-//        List<User> users = session.createNativeQuery("select * from user_table", User.class).getResultList();
-//        List<User> users =
-//                session
-//                        .createQuery("select u from User u where  u.id>:id and u.name=:name", User.class)
-//                        .setParameter("id", 2)
-//                        .setParameter("name","vasya")
-//                        .getResultList();
-//        System.out.println(users);
-
-//        User user = session.find(User.class, 1);
-//        System.out.println(user);
-//        user.setName("utwqeuqwe");
-//
-//        session.beginTransaction();
-////        session.update(user);
-//
-//        session.delete(user);
-//        session.getTransaction().commit();
-
-
-        //        List<Passport> resultList = session.createQuery("select u.passport from User u", Passport.class).getResultList();
-//        System.out.println(resultList);
-
-        List<User> users = session.createQuery("select u from User u", User.class).getResultList();
-        System.out.println(users.get(0).getPassport());
         session.close();
         sessionFactory.close();
 
--- src/main/java/models/Gendrer.java
+++
@@ -1,6 +0,0 @@
-package models;
-
-public enum Gendrer {
-
-    MALE, FEMALE;
-}
