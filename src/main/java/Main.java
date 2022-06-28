4032775cbff95f0f30b040ba7caf1c7d792d263e
GrayHead <ss.zhuravlov@gmail.com>
2022-06-28 11:53:25 +0300

    hw 1-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-import models.Gendrer;
-import models.Passport;
-import models.User;
+import models.Word;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
@@ -8,21 +6,28 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-        Metadata metadata = new MetadataSources(serviceRegistry).addAnnotatedClass(null).addAnnotatedClass(null).getMetadataBuilder().build();
+        Metadata metadata = new MetadataSources(serviceRegistry).addAnnotatedClass(Word.class).getMetadataBuilder().build();
         SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
         Session session = sessionFactory.openSession();
 
         session.beginTransaction();
 
+        session.save(new Word());
+        session.save(new Word());
+        session.save(new Word());
+        session.save(new Word());
+        session.save(new Word());
 
         session.getTransaction().commit();
+
+        List<Word> words = session.createQuery("select w from Word w", Word.class).getResultList();
+        System.out.println(words);
 
         session.close();
         sessionFactory.close();
