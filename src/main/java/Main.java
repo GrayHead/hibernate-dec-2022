15cb97b31accc90c562c8893195562b98b8d80f4
GrayHead <ss.zhuravlov@gmail.com>
2023-07-10 21:24:22 +0300

    1 1 uni

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import models.Gender;
+import models.Passport;
 import models.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -8,7 +9,6 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +19,7 @@ public class Main {
 
         Metadata metadata = new MetadataSources(serviceRegistry)
                 .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Passport.class)
                 .getMetadataBuilder()
                 .build();
 
@@ -26,22 +27,30 @@ public class Main {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        User user1 = new User("vasya", "vasya@asd.com", Arrays.asList("java", "js"), Gender.MALE);
-        session.save(user1);
-        User user2 = new User("petya", "petya@asd.com", Arrays.asList("java", "js"), Gender.MALE);
+        User user1 = new User("vasya",
+                "vasya@asd.com",
+                Arrays.asList("java", "js"),
+                Gender.MALE,
+                new Passport("831t57wtf8g"));
+
+        session.save(user1); // id-1
+
+        User user2 = new User("petya",
+                "petya@asd.com",
+                Arrays.asList("java", "js"),
+                Gender.MALE,
+                new Passport("yasfdytfdy"));
         session.save(user2);
-        User user3 = new User("anna", "anna@asd.com", Arrays.asList("java", "js"), Gender.FEMALE);
-        session.save(user3);
 
         session.getTransaction().commit();
 
-//        List<User> users = session.createNativeQuery("select  * from user_table", User.class).getResultList();
+        session.createQuery("select u from  User u", User.class).getResultList().forEach(System.out::println);
 
-//        List<User> users = session.createQuery("select u from User u", User.class).getResultList();
-//        System.out.println(users);
 
-        List<Integer> idList = session.createQuery("select x.id from User x", Integer.class).getResultList();
-        System.out.println(idList);
+        session.getTransaction().begin();
+        session.delete(user1);
+
+        session.getTransaction().commit();
         session.close();
         sessionFactory.close();
 
--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -25,10 +25,22 @@ public class User {
     @Enumerated(EnumType.STRING)
     private Gender gender;
 
+    @OneToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name =  "passport_id",referencedColumnName = "id")
+    private Passport passport;
+
     public User(String name, String email, List<String> skills, Gender gender) {
         this.name = name;
         this.email = email;
         this.skills = skills;
         this.gender = gender;
+    }
+
+    public User(String name, String email, List<String> skills, Gender gender, Passport passport) {
+        this.name = name;
+        this.email = email;
+        this.skills = skills;
+        this.gender = gender;
+        this.passport = passport;
     }
 }
